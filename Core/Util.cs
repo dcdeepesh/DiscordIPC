3dbdafaa11856194600579a0de9f4a72a55319d3
Deepesh Choudhary <dcdeepesh7@gmail.com>
2021-07-21 10:14:08 +0530

    Refactor

---
+++ Core/Util.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Text.Json;
+
+namespace Dec.DiscordIPC {
+    internal static class Extensions {
+        public static T ToObject<T>(this JsonElement element) =>
+            JsonSerializer.Deserialize<T>(element.GetRawText());
+    }
+
+    internal class Util {
+        public static void Log(string msg) => Console.WriteLine(msg);
+        public static void Log(string format, params object[] arg) =>
+            Console.WriteLine(format, arg);
+    }
+}
