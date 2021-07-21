c0e6620f6dede32db2d1d69f3b14bb029035f3bc
Deepesh Choudhary <dcdeepesh7@gmail.com>
2021-07-21 10:14:08 +0530

    Refactor

--- Core/Extensions.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Text.Json;
-
-namespace Dec.DiscordIPC {
-    internal static class Extensions {
-        public static T ToObject<T>(this JsonElement element) =>
-            JsonSerializer.Deserialize<T>(element.GetRawText());
-    }
-}
