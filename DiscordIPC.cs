de0429f22e10832b9a7ab44641dbb78569907947
Deepesh Choudhary <dcdeepesh7@gmail.com>
2022-12-10 15:03:54 +0530

    Use new implementation of SendCommandAsync()
    Remove all overloads, and add a new,
    generic implementation.

--- DiscordIPC.cs
+++ DiscordIPC.cs
@@ -25,65 +25,31 @@ namespace Dec.DiscordIPC {
 
         #region Commands
 
-        public async Task<AuthorizeCommand.Data> SendCommandAsync(AuthorizeCommand.Args args) =>
-            await SendCommandAsync_Core<AuthorizeCommand.Data>("AUTHORIZE", args);
-
-        public async Task<AuthenticateCommand.Data> SendCommandAsync(AuthenticateCommand.Args args) =>
-            await SendCommandAsync_Core<AuthenticateCommand.Data>("AUTHENTICATE", args);
-
-        public async Task<GetGuildCommand.Data> SendCommandAsync(GetGuildCommand.Args args) =>
-            await SendCommandAsync_Core<GetGuildCommand.Data>("GET_GUILD", args);
-
-        public async Task<GetGuildsCommand.Data> SendCommandAsync(GetGuildsCommand.Args _) =>
-            await SendCommandAsync_Core<GetGuildsCommand.Data>("GET_GUILDS", null);
-
-        public async Task<GetChannelCommand.Data> SendCommandAsync(GetChannelCommand.Args args) =>
-            await SendCommandAsync_Core<GetChannelCommand.Data>("GET_CHANNEL", args);
-
-        public async Task<GetChannelsCommand.Data> SendCommandAsync(GetChannelsCommand.Args args) =>
-            await SendCommandAsync_Core<GetChannelsCommand.Data>("GET_CHANNELS", args);
-
-        public async Task<SetUserVoiceSettingsCommand.Data> SendCommandAsync(SetUserVoiceSettingsCommand.Args args) =>
-            await SendCommandAsync_Core<SetUserVoiceSettingsCommand.Data>("SET_USER_VOICE_SETTINGS", args);
-
-        public async Task<SelectVoiceChannelCommand.Data> SendCommandAsync(SelectVoiceChannelCommand.Args args) =>
-             await SendCommandAsync_Core<SelectVoiceChannelCommand.Data>("SELECT_VOICE_CHANNEL", args);
-
-        public async Task<GetSelectedVoiceChannelCommand.Data> SendCommandAsync(GetSelectedVoiceChannelCommand.Args _) =>
-            await SendCommandAsync_Core<GetSelectedVoiceChannelCommand.Data>("GET_SELECTED_VOICE_CHANNEL", null);
-
-        public async Task<SelectTextChannelCommand.Data> SendCommandAsync(SelectTextChannelCommand.Args args) =>
-            await SendCommandAsync_Core<SelectTextChannelCommand.Data>("SELECT_TEXT_CHANNEL", args);
-
-        public async Task<GetVoiceSettingsCommand.Data> SendCommandAsync(GetVoiceSettingsCommand.Args _) =>
-            await SendCommandAsync_Core<GetVoiceSettingsCommand.Data>("GET_VOICE_SETTINGS", null);
-
-        public async Task<SetVoiceSettingsCommand.Data> SendCommandAsync(SetVoiceSettingsCommand.Args args) =>
-            await SendCommandAsync_Core<SetVoiceSettingsCommand.Data>("SET_VOICE_SETTINGS", args);
-
-        public async Task SendCommandAsync(SetCertifiedDevicesCommand.Args args) =>
-            await SendCommandAsync_Core<object>("SET_CERTIFIED_DEVICES", args, false);
-
-        public async Task SendCommandAsync(SetActivityCommand.Args args) =>
-            await SendCommandAsync_Core<object>("SET_ACTIVITY", args, false);
-
-        public async Task SendCommandAsync(SendActivityJoinInviteCommand.Args args) =>
-            await SendCommandAsync_Core<object>("SET_ACTIVITY_JOIN_INVITE", args, false);
-
-        public async Task SendCommandAsync(CloseActivityRequestCommand.Args args) =>
-            await SendCommandAsync_Core<object>("CLOSE_ACTIVITY_REQUEST", args, false);
-
-        private async Task<T> SendCommandAsync_Core<T>(
-                string cmd, dynamic args, bool convert = true) {
+        public async Task<TData> SendCommandAsync<TArgs, TData>(ICommand<TArgs, TData> command) {
             var nonce = Guid.NewGuid().ToString();
             dynamic payload;
+            var args = command.Arguments;
+            string cmd = command.Name;
             if (args is null)
                 payload = new { cmd, nonce };
             else
                 payload = new { cmd, nonce, args };
-
+        
             JsonElement response = await SendCommandWeakTypeAsync(payload);
-            return convert ? response.GetProperty("data").ToObject<T>() : default;
+            return response.GetProperty("data").ToObject<TData>();
+        }
+        
+        public async Task SendCommandAsync<TArgs>(ICommand<TArgs> command) {
+            var nonce = Guid.NewGuid().ToString();
+            dynamic payload;
+            var args = command.Arguments;
+            string cmd = command.Name;
+            if (args is null)
+                payload = new { cmd, nonce };
+            else
+                payload = new { cmd, nonce, args };
+            
+            await SendCommandWeakTypeAsync(payload);
         }
 
         #endregion
