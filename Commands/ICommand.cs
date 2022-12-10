d3cb4d9c112f14656014b8c9060ae2e4746c3e51
Deepesh Choudhary <dcdeepesh7@gmail.com>
2022-12-10 13:56:33 +0530

    Add Arguments property to ICommand

--- Commands/ICommand.cs
+++ Commands/ICommand.cs
@@ -1,7 +1,9 @@
 ﻿namespace Dec.DiscordIPC.Commands {
     public interface ICommand<TArgs> {
+        TArgs Arguments { get; set; }
     }
     
     public interface ICommand<TArgs, TData> {
+        TArgs Arguments { get; set; }
     }
 }
