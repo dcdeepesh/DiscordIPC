eefd55adb6d30d7e8ee906cbd6690f0f17d36288
Deepesh Choudhary <dcdeepesh7@gmail.com>
2022-12-15 16:16:56 +0530

    Remove unused using directives

--- DiscordIPC/Core/Dispatcher.cs
+++ DiscordIPC/Core/Dispatcher.cs
@@ -1,6 +1,5 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text.Json;
 using System.Threading;
 
 namespace Dec.DiscordIPC.Core; 
--- DiscordIPC/Core/IpcHandler.cs
+++ DiscordIPC/Core/IpcHandler.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.IO;
 using System.IO.Pipes;
-using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
 
