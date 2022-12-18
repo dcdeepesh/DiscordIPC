4790643cda50e76bd51fea3d8a6594f433171341
Deepesh Choudhary <dcdeepesh7@gmail.com>
2022-12-18 22:08:56 +0530

    Add ILogger to DiscordIpcClientOptions

--- DiscordIPC/DiscordIpcClientOptions.cs
+++ DiscordIPC/DiscordIpcClientOptions.cs
@@ -1,7 +1,11 @@
-﻿namespace Dec.DiscordIPC; 
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+
+namespace Dec.DiscordIPC; 
 
 public class DiscordIpcClientOptions {
     public int PipeNumber { get; set; } = 0;
     // TODO: implement this limit
     public int ResponsePoolLimit { get; set; } = 32;
+    public ILogger Logger { get; set; } = NullLogger.Instance;
 }
